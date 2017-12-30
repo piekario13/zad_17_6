8eb7244560e64d48ddfaa30c19aea8819717fbd9
piekario13 <piekario13@gmail.com>
2017-12-30 21:11:13 +0100

    zad17_6

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views','./views');
+
+app.get('/', function(req, res) {
+    res.render('page1');
+});
+
+app.get('/auth/google', function(req, res) {
+    res.render('page2');
+});
+
+app.listen(3000);
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
